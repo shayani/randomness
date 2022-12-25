5ca2bfdadbd0717fd217056f4c1c1570b32219be
Fernando Shayani <shayani@gmail.com>
2022-12-25 07:42:50 -0300

    Improvments of Ruby code

--- ruby.rb
+++ ruby.rb
@@ -1,17 +1,18 @@
 random = Random.new
 
 test_size = 1_000_000
-
-result_array = []
+zeros = 0
+ones = 0
 
 (1..test_size).each do |i|
-  result_array << random.rand(2)
+  if random.rand(2) == 0
+    zeros += 1
+  else
+    ones += 1
+  end
 end
 
-zeros = result_array.count(0)
 zeros_percentage = (zeros.to_f / test_size.to_f * 100.0).round(3)
-
-ones = result_array.count(1)
 ones_percentage = (ones.to_f / test_size.to_f * 100.0).round(3)
 
 puts "Generated #{test_size} random samples and got"
